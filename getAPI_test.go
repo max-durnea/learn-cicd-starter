f14d8908bb131349bd6b3ebedf3ee3a4d6804901
max-durnea <maxim.durnea@mail.ru>
2025-10-05 19:28:38 +0300

    Add Style check

--- getAPI_test.go
+++ getAPI_test.go
@@ -1,27 +1,28 @@
 package main
 
-import ("testing"
-	"net/http"
+import (
 	"github.com/bootdotdev/learn-cicd-starter/internal/auth"
+	"net/http"
+	"testing"
 )
 
-func TestGETapiKey(t *testing.T){
+func TestGETapiKey(t *testing.T) {
 	header := http.Header{}
 	const apiKey string = "1234"
-	header.Set("Authorization","ApiKey"+" "+apiKey)
-	result,err:= auth.GetAPIKey(header)
+	header.Set("Authorization", "ApiKey"+" "+apiKey)
+	result, err := auth.GetAPIKey(header)
 	if err != nil {
-		t.Errorf("Unexpected Error:%v",err)
+		t.Errorf("Unexpected Error:%v", err)
 	}
 	if result != apiKey {
-		t.Errorf("Key was not extracted correctly: Expected %v, Got %v",apiKey,result)
+		t.Errorf("Key was not extracted correctly: Expected %v, Got %v", apiKey, result)
 	}
 }
-func TestGETapiKeyWrong(t *testing.T){
+func TestGETapiKeyWrong(t *testing.T) {
 	header := http.Header{}
 	const apiKey string = "1234"
-	header.Set("Authorization","ApisKey"+" "+apiKey)
-	result,err:= auth.GetAPIKey(header)
+	header.Set("Authorization", "ApisKey"+" "+apiKey)
+	result, err := auth.GetAPIKey(header)
 	if err != nil {
 		return
 	}

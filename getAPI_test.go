e3784031885f43cb4df170b23c62b9dc94fd8fc1
max-durnea <maxim.durnea@mail.ru>
2025-10-05 17:25:54 +0300

    Add tests for authentication

---
+++ getAPI_test.go
@@ -0,0 +1,31 @@
+package main
+
+import ("testing"
+	"net/http"
+	"github.com/bootdotdev/learn-cicd-starter/internal/auth"
+)
+
+func TestGETapiKey(t *testing.T){
+	header := http.Header{}
+	const apiKey string = "1234"
+	header.Set("Authorization","ApiKey"+" "+apiKey)
+	result,err:= auth.GetAPIKey(header)
+	if err != nil {
+		t.Errorf("Unexpected Error:%v",err)
+	}
+	if result != apiKey {
+		t.Errorf("Key was not extracted correctly: Expected %v, Got %v",apiKey,result)
+	}
+}
+func TestGETapiKeyWrong(t *testing.T){
+	header := http.Header{}
+	const apiKey string = "1234"
+	header.Set("Authorization","ApisKey"+" "+apiKey)
+	result,err:= auth.GetAPIKey(header)
+	if err != nil {
+		return
+	}
+	if result == apiKey {
+		t.Errorf("Key should have not been extracted")
+	}
+}
